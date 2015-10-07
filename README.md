33be06b6821d676a7503654de92993d2aabb5ab1
Alan Dwi Prasetyo <alandwiprasetyo@gmail.com>
2015-10-07 10:43:57 +0700

    change README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Android-SQLite
 
 This project is just one example of the use of SQLite on android. (Android Studio)
-Happy Coding !
+<br>Happy Coding !
--- app/src/main/java/com/sliding/root/sqliteandroid/MainActivity.java
+++ app/src/main/java/com/sliding/root/sqliteandroid/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends ListActivity  implements View.OnClickListener{
     Button btnAdd,btnGetAll;
     TextView student_Id;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
